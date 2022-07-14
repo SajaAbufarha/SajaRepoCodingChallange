980c152953abca2a3ca1a34ca1f96fe4b425c23a
li23 <li23@IC-CS233-5.uc.ucalgary.ca>
2022-07-14 16:53:59 -0600

    Changed type of root to VBox and added a label to the window.

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 
 
