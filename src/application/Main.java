b1e43087f71d375ab44740070203800d24fd797e
arush.sanghal <arush.sanghal@IC-CS233-20.uc.ucalgary.ca>
2022-07-14 17:07:31 -0600

    Added my branch label.

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,9 +12,9 @@ public class Main extends Application {
 		try {
 			VBox root = new VBox();
 
-			root.getChildren().add(new Label("Ran's label"));
 
 			root.getChildren().add(new Label("Arush's label"));
+			root.getChildren().add(new Label("Arush's branch"));
 			Scene scene = new Scene(root, 400, 400);
 			// scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
